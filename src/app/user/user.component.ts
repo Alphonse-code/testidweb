7bc5485c14d8acfd492e55f399614ed1eaa13be5
Mialy <rakotozanany.mi@gmail.com>
2023-05-19 15:16:09 +0300

    new routes

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -2,9 +2,9 @@ import { Component, Input, OnInit } from '@angular/core';
 import { DataService, User } from '../services/data.service';
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss'],
+    selector: 'app-user',
+    templateUrl: './user.component.html',
+    styleUrls: ['./user.component.scss'],
 })
 export class UserComponent implements OnInit {
   @Input() user?: User;
